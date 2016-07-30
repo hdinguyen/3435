b2d4e132274d6a6ff0148d985bbcb28b0c1a69d5
nguyen huynh <nguyen.huynh@butterflyhub.com>
2016-07-30 10:23:42 +0700

    Init ios project

---
+++ fbhackathon/fbhackathon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fbhackathon
+//
+//  Created by Nguyenh on 7/30/16.
+//  Copyright © 2016 3435. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
